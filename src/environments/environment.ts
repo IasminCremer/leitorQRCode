4281b02639b26b5f7f9116040c2cb0430d367e6c
IasminCremer <ias.cremer@gmail.com>
2020-10-29 23:46:45 -0300

    leitor QRCode
    finalizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDVZOW9lHWKenNT-HVN88t8j6MknkmY528",
+    authDomain: "leitorqrcodefbase.firebaseapp.com",
+    databaseURL: "https://leitorqrcodefbase.firebaseio.com",
+    projectId: "leitorqrcodefbase",
+    storageBucket: "leitorqrcodefbase.appspot.com",
+    messagingSenderId: "952226974475",
+    appId: "1:952226974475:web:8b4d7551c5ddf1405c300e"
+  }
 };
 
 /*
